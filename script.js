6061d4946e09fc5837bd3f4e01058fce3f1c1c74
George Francis <g.francis@Geos-Air.fios-router.home>
2020-06-10 11:40:56 -0400

    changes

--- script.js
+++ script.js
@@ -1,41 +1,86 @@
-let body = document.querySelector('body'); //create a variable and set it to html document body
-body.setAttribute('style', 'color: teal'); // set the text color of the body (and all its elements)
+// let body = document.querySelector('body'); //create a variable and set it to html document body
+// body.setAttribute('style', 'color: teal'); // set the text color of the body (and all its elements)
 
-let newButton = document.createElement('button'); // create a variable and set it to a new button
-body.appendChild(newButton); // add button to the body
-newButton.innerHTML = "ADD"; // adding text to the button 
+// let newButton = document.createElement('button'); // create a variable and set it to a new button
+// body.appendChild(newButton); // add button to the body
+// newButton.innerHTML = "ADD"; // adding text to the button 
 
+// let i = 0;
+
+// function addButtonClicked() {
+//     let newHeader = document.createElement('h2');
+
+//     newHeader.innerHTML = "The World Is Yours";
+//     newHeader.setAttribute('id', i);
+//     body.appendChild(newHeader);
+
+//     i++;
+      
+// }
+
+
+// newButton.addEventListener('click', addButtonClicked);
+
+// let newButton2 = document.createElement('button'); // create a variable and set it to a new button
+// body.appendChild(newButton2); // add button to the body
+// newButton2.innerHTML = "Remove"; // adding text to the button 
+
+// function removeButtonClicked() {
+//    toDelete = document.getElementById(i - 1);
+//    console.log(toDelete);
+//    body.removeChild(toDelete);
+
+//    i--;
+   
+// }
+
+// newButton2.addEventListener('click', removeButtonClicked);
+
+
+// //   console.log(body);
+
+
+
+
+
+
+
+
+
+let body = document.querySelector('body'); 
+let newButton = document.createElement('button');
+let newButton2 = document.createElement('button'); 
 let i = 0;
 
-function addButtonCLicked() {
-    // console.log('hit');    //:These two lines check if your function works
-    // alert("Good job clicking the button")
+body.setAttribute('style', 'color: teal'); 
+body.appendChild(newButton); 
+body.appendChild(newButton2); 
+newButton.innerHTML = "ADD"; 
+newButton2.innerHTML = "Remove"; 
 
+newButton.addEventListener('click', addButtonClicked);
+newButton2.addEventListener('click', removeButtonClicked);
+
+function addButtonClicked() {
     let newHeader = document.createElement('h2');
+
     newHeader.innerHTML = "The World Is Yours";
     newHeader.setAttribute('id', i);
     body.appendChild(newHeader);
 
-    console.log(newHeader);    
+    i++;
+      
+}
+
+function removeButtonClicked() {
+   toDelete = document.getElementById(i - 1);
+   console.log(toDelete);
+   body.removeChild(toDelete);
+
+   i--;
+   
 }
 
 
-newButton.addEventListener('click', addButtonCLicked);
-
-let newButton2 = document.createElement('button'); // create a variable and set it to a new button
-body.appendChild(newButton2); // add button to the body
-newButton2.innerHTML = "Remove"; // adding text to the button 
-
-function removeButtonCLicked() {
-    // console.log('hit');    //:These two lines check if your function works
-    // alert("Good job clicking the button")
-
-    let newHeader = document.createElement('h2');
-    newHeader.innerHTML = "The World Is Yours";
-    body.appendChild(newHeader);
-}
-
-newButton2.addEventListener('click', removeButtonCLicked);
 
 
-//   console.log(body);
